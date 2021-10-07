1da76edf8bc16a10b76aabd8c2766c937b41a1d4
CopyandPaste-png <79705748+CopyandPaste-png@users.noreply.github.com>
2021-10-07 12:09:01 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,14 @@
 
 int main()
 {
-    std::string token = "BQC-2FVNzili-iPgdOUKGGO0ig3E1XOPNZ8iiaiozmrSg0H1m6tYy0hJaMIVH0qq9Xc1_kZKl2kcGRJDsMUvsLDrtG8qUiRP3ovf0nNZA2qFaml0V3IrU0L30BybtHRcvDlXrSDQnymG0mjrxZSEvqUa6ZK5BbZmGz3HHRTDHZI";
+    std::string token = "";
     
     Spotify test = Spotify(token,
-        "8939a122f64f4d89bdb9d457f3457a2b",
-        "25708c369d9f4e43be6680dc3fdd1e90",
-        "https://www.google.com");
+        "",
+        "",
+        "");
 
-    std::string scopes = "ugc-image-upload user-library-modify user-read-recently-played gay-boy";
+    std::string scopes = "";
     test.basicauthorize(scopes);
     std::cin.get();
     return 0;
