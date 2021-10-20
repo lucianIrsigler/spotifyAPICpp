2063d27ed29736df401bcb2ea31ec472b9cddc98
CopyandPaste-png <l.irsigler64@gmail.com>
2021-10-20 14:00:30 +0200

    function for logging to a file

---
+++ source/v1.1/spotifylogging.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <string>
+#include <fstream>
+
+
+static void spotifyLogToFile(std::string message) {
+    char filename[] = "SPOTIFYLOG.txt";
+    std::ofstream file;
+    file.open(filename, std::fstream::app);
+    file << "\n" << message << "\n";
+    file.close();
+}
