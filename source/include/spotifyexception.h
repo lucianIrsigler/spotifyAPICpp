f59dd342eb86456e0ff37a97673779b9cc3b97f2
CopyandPaste-png <l.irsigler64@gmail.com>
2022-01-27 14:09:03 +0200

    more utility functions

---
+++ source/include/spotifyexception.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <exception>
+#include <string>
+
+/*
+	Spotify exception
+*/
+class spotifyException : public std::exception
+{
+	std::string _msg;
+public:
+	spotifyException(const std::string& message): _msg(message){}
+
+	virtual const char* what() const noexcept override
+	{
+		return _msg.c_str();
+	}
+};
